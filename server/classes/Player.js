1441dd3df2b5c9d9d36f67d2322e2887f614fd15
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-04-20 22:00:30 +0530

    feat: add palyer class

---
+++ server/classes/Player.js
@@ -0,0 +1,25 @@
+class Player {
+    constructor(id,name,isHost = false){
+        this.id = id;
+        this.name = name;
+        this.score = 0;
+        this.isHost = isHost;
+        this.hasGuessed = false;
+    }
+    addScore(points){
+        this.score += points;
+    }
+    resetGuess(){
+        this.hasGuessed = false;
+    }
+    toJSON(){
+        return{
+            id:this.id,
+            name:this.name,
+            score:this.score,
+            isHost:this.isHost,
+            hasGuessed:this.hasGuessed
+        }
+    }
+}
+module.exports = Player;
