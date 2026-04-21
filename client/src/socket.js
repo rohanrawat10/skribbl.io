ab22e423739f045d2ee169583eb1ff34b4b19387
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-04-21 12:50:43 +0530

    updated frontend url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
+const SOCKET_URL = "https://skribbl-io-kppl.onrender.com";
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
